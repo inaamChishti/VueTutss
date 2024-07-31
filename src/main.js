bc7f156ad578b4f0543bde66559b2a1a4e2305dc
inaamChishti <inaam.chishti2@gmail.com>
2024-08-01 00:23:42 +0500

    udpated code

--- src/main.js
+++ src/main.js
@@ -3,13 +3,7 @@ import App from './App.vue'
 import router from './routes';
 
 createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
+
 
 
 
