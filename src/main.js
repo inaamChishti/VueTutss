53e3c037c08d6f6a421a6fba0eaa907790e0fa94
inaamChishti <inaam.chishti2@gmail.com>
2024-08-01 02:17:55 +0500

    i am new coommit

--- src/main.js
+++ src/main.js
@@ -3,26 +3,10 @@ import App from './App.vue'
 import router from './routes';
 
 createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
 
 
 
 
-// Dev Inaam branch code
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
 
 
 
-
-// Dev Inaam branch code
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-createApp(App).use(router).mount('#app')
-
-
-
-
-// Dev Inaam branch code
